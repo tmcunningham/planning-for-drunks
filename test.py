46dd0ccd5ce557d0657bff5fdbb3677032b145b7
tmcunningham <80322877+tmcunningham@users.noreply.github.com>
2021-09-28 17:31:07 +0100

    Create list of drunks

--- test.py
+++ test.py
@@ -7,22 +7,23 @@ Created on Mon Sep 27 15:19:45 2021
 
 import csv
 import matplotlib
+import operator
 
 with open("drunk.plan", newline = "") as f:
     reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    data = []
+    town = []
     for row in reader:
         rowlist = []
         for value in row:
             rowlist.append(value)
-        data.append(rowlist)
+        town.append(rowlist)
 
 # Check data has correct dimensions
-#len(data)
-#len(data[0])
+#len(town)
+#len(town[0])
 
 # Plot data        
-matplotlib.pyplot.imshow(data)
+matplotlib.pyplot.imshow(town)
 
 #Create empty dictionary to collect building co-ordinates
 building_coords = {}
@@ -35,26 +36,36 @@ for n in [1, *range(10, 260, 10)]:
     else:
         building_name = n
     building_coords[building_name] = []
-    for y in range(len(data)):
-        for x in range(len(data[y])):
-            if data[y][x] == n:
+    for y in range(len(town)):
+        for x in range(len(town[y])):
+            if town[y][x] == n:
                 building_coords[building_name].append((x, y))
+                          
+
+pub_door_y = min(building_coords["pub"], key = operator.itemgetter(1))[0]
+
+
+building_coords["pub"]
                 
+drunks = []
+
+for id in range(10, 260, 10):
+    drunks.append()
 
 
 
-"""
+
+
+
 # Plot the pub to verify it is larger than other buildings
-data2 = data
+town2 = town
 
-for i in range(len(data2)):
-    for j in range(len(data2[i])):
-        if data[i][j] == 1:
-            data[i][j] = 200
-        else:
-            data[i][j] = 0
+for i in range(len(town2)):
+    for j in range(len(town2[i])):
+        if town2[i][j] == 1:
+            town2[i][j] = 400
         
-matplotlib.pyplot.imshow(data2)
-"""
+matplotlib.pyplot.imshow(town2)
+
 
 
