a623e1d32dee8facbffdb7192d97bc7ffd3d8a29
tmcunningham <80322877+tmcunningham@users.noreply.github.com>
2021-11-29 16:27:35 +0000

    Fix issue with timing

--- python/measure_drunks_moves.py
+++ python/measure_drunks_moves.py
@@ -10,7 +10,7 @@ import matplotlib
 import timeit
 
 # Set start time to time programme
-start_time = timeit.default_timer
+start_time = timeit.default_timer()
 
 # Set maximum number of moves (if this is too low it will affect results)
 max_moves = 50000000000
@@ -32,7 +32,7 @@ drunk_levels = [10, 20, 50, 100, 200]
 
 # Set number of iterations to run model for each drunk_level
 iterations = 1000
-
+    
 # Create empty list to store move counts
 drunk_level_moves = []
 
@@ -66,6 +66,7 @@ matplotlib.pyplot.boxplot(drunk_level_moves, positions = drunk_levels)
 
 # Set end time
 end_time = timeit.default_timer()
+time_taken = end_time - start_time
 
 # Calculate time taken to run
-print("Time taken: " + str(end_time - start_time))
+print("Time taken: " + str(time_taken))
